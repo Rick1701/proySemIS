c65004e1086e58ee79485f5bdccc4c81b2825f4b
Ricardo_Flores <ricardo.flores1701@alumnos.ubiobio.cl>
2022-11-09 22:25:05 -0300

    dependencia producto - categoria

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,12 +1,13 @@
 const Product = require('../models/product');
 
 const createProduct = (req, res) => {
-    const {name,price,quantity,description} = req.body;
+    const {name,price,quantity,description,category} = req.body;
     const newProduct = new Product({
         name,
         price,
         quantity,
-        description
+        description,
+        category
     });
 
     //INGRESAN LOS PARAMETROS NUEVOS Y SE COMPARA CON ERROR, SI NO EXISTE ERROR SE GUARDA
@@ -21,17 +22,16 @@ const createProduct = (req, res) => {
 const getProducts = (req, res) => {
     Product.find({},(err,product) => {
         if(err){
-            return res.status(400).send({message:"Error al obtener el producto!!"})
+            return res.status(400).send({message:"Error al obtener los productos!!"})
         }
         return res.status(200).send(product)
     })
 }
-
+/*
 //CONTROLADOR PARA OBTENER UN SOLO PRODUCTO COMM
 const getSpecificProduct = (req,res) =>{
     //DE AQUI EXTRAIGO EL PARAMETRO QUE ENVIARÉ PARA LA RUTA (id)
     const {id} = req.params;
-    /*console.log(id);*/
     Product.findById(id, (err, products) =>{
         if(err){
             return res.status(400).send({message:"Error al obtener el producto!!"})
@@ -42,7 +42,25 @@ const getSpecificProduct = (req,res) =>{
         //CONSIDERAR BUSQUEDA EN LA PLURALIDAD DE PRODUCTOS (productSSSSSSS)
         return res.status(200).send(products)
     })
+}*/
+
+
+
+const getSpecificProduct = (req,res) =>{
+    //DE AQUI EXTRAIGO EL PARAMETRO QUE ENVIARÉ PARA LA RUTA (id)
+    const {id} = req.params;
+    Product.findById(id).populate({ path: 'category' }).exec((err, product) =>{
+        if(err){
+            return res.status(400).send({ message:"Error al obtener el producto!!" })
+        }
+        if(!product){
+            return res.status(404).send({ message: "Producto no encontrado!!"})
+        }
+        //CONSIDERAR BUSQUEDA EN LA PLURALIDAD DE PRODUCTOS (productSSSSSSS)
+        return res.status(200).send(product)
+    })
 }
+
 
 //CONTROLADOR PARA ACTUALIZAR UN SOLO PRODUCTO COMM
 const updateProduct = (req,res) =>{
